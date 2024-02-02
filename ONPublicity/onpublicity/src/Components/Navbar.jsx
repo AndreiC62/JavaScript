5299888a7aadec984faf767d1e510d24c93fa110
AndreiC <andrei.cazac@yahoo.com>
2024-02-02 21:15:35 +0200

    update 02 februarie

--- ONPublicity/onpublicity/src/Components/Navbar.jsx
+++ ONPublicity/onpublicity/src/Components/Navbar.jsx
@@ -1,21 +1,72 @@
-import React from "react";
-import "./Navbar.css";
+import React, { useState } from "react";
 import MyLogo from "./logo.svg";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <img src={MyLogo} alt="Logo" className="logo" />
+    <div>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <img src={MyLogo} alt="Logo" className="logo" />
+        </div>
+        <div className={`navbar-center ${isMenuOpen ? "show" : ""}`}>
+          <a href="/" onClick={toggleMenu}>
+            Acasă
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Servicii
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Despre noi
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Contactează-ne
+          </a>
+        </div>
+        <div className="navbar-right">
+          <div
+            className={`hamburger-icon ${isMenuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className="language">
+          <p>RO / EN</p>
+        </div>
+      </nav>
+      <div className={`link-container ${isMenuOpen ? "show" : ""}`}>
+        <a href="/" className="link-meniu">
+          Acasă
+        </a>
+        <a href="/" className="link-meniu">
+          Servicii
+        </a>
+        <a href="/" className="link-meniu">
+          Despre noi
+        </a>
+        <a href="/" className="link-meniu">
+          Contactează-ne
+        </a>
+
+        <br></br>
+
+        <a href="/" className="meniu-mail">
+          <FontAwesomeIcon icon={faEnvelope} className="icon1" />
+          onpublicity@gmail.com
+        </a>
       </div>
-      <div className="navbar-center">
-        <a href="/">Link 1</a>
-        <a href="/">Link 2</a>
-        <a href="/">Link 3</a>
-        <a href="/">Link 4</a>
-      </div>
-      <div className="navbar-right"></div>
-    </nav>
+    </div>
   );
 };
 
